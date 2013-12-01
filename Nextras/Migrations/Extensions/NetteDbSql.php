43522a6d7f5f453e3f225fedd0dbec209c4f6459
hrach <hrach.cz@gmail.com>
2013-12-01 17:21:15 +0100

    NetteDbSql extension: fixed parsing PostgreSQL procedures & triggers

--- Nextras/Migrations/Extensions/NetteDbSql.php
+++ Nextras/Migrations/Extensions/NetteDbSql.php
@@ -2,6 +2,9 @@
 namespace Nextras\Migrations\Extensions;
 
 use Nette\Database\Context;
+use Nette\Database\Drivers\MsSqlDriver;
+use Nette\Database\Drivers\PgSqlDriver;
+use Nette\Database\Drivers\SqliteDriver;
 use Nextras\Migrations\Entities\File;
 use Nextras\Migrations\IExtensionHandler;
 use Nextras\Migrations\IOException;
@@ -10,6 +13,7 @@ use Nextras\Migrations\LogicException;
 
 /**
  * @author Petr Procházka
+ * @author Jan Skrasek
  */
 class NetteDbSql implements IExtensionHandler
 {
@@ -56,24 +60,38 @@ class NetteDbSql implements IExtensionHandler
 	 * @author   Jakub Vrána
 	 * @author   Jan Tvrdík
 	 * @author   Michael Moravec
+	 * @author   Jan Skrasek
 	 * @license  Apache License
 	 */
 	protected function loadFile($file)
 	{
 		$query = @file_get_contents($file);
-		if (!$query) throw new IOException("Cannot open file '$file'.");
+		if (!$query) {
+			throw new IOException("Cannot open file '$file'.");
+		}
 
 		$delimiter = ';';
-		$offset = 0;
-		$queries = 0;
+		$offset = $queries = 0;
+		$space = "(?:\\s|/\\*.*\\*/|(?:#|-- )[^\\n]*\\n|--\\n)";
+
+		$driver = $this->context->getConnection()->getSupplementalDriver();
+		if ($driver instanceof MsSqlDriver) {
+			$parse = '[\'"[]|/\*|-- |$';
+		} elseif ($driver instanceof SqliteDriver) {
+			$parse = '[\'"`[]|/\*|-- |$';
+		} elseif ($driver instanceof PgSqlDriver) {
+			$parse = '[\'"]|/\*|-- |$|\$[^$]*\$';
+		} else {
+			$parse = '[\'"`#]|/\*|-- |$';
+		}
 
 		while ($query != '') {
-			if (!$offset && preg_match('~^\\s*DELIMITER\\s+(.+)~i', $query, $match)) {
+			if (!$offset && preg_match("~^{$space}*DELIMITER\\s+(\\S+)~i", $query, $match)) {
 				$delimiter = $match[1];
 				$query = substr($query, strlen($match[0]));
 
 			} else {
-				preg_match('(' . preg_quote($delimiter) . '|[\'`"]|/\\*|-- |#|$)', $query, $match, PREG_OFFSET_CAPTURE, $offset); // should always match
+				preg_match('(' . preg_quote($delimiter) . "\\s*|$parse)", $query, $match, PREG_OFFSET_CAPTURE, $offset); // should always match
 				$found = $match[0][0];
 				$offset = $match[0][1] + strlen($found);
 
@@ -81,7 +99,7 @@ class NetteDbSql implements IExtensionHandler
 					break;
 				}
 
-				if (!$found || $found == $delimiter) { // end of a query
+				if (!$found || rtrim($found) == $delimiter) { // end of a query
 					$q = substr($query, 0, $match[0][1]);
 
 					$queries++;
@@ -91,7 +109,7 @@ class NetteDbSql implements IExtensionHandler
 					$offset = 0;
 
 				} else { // find matching quote or comment end
-					while (preg_match('~' . ($found == '/*' ? '\\*/' : (preg_match('~-- |#~', $found) ? "\n" : "$found|\\\\.")) . '|$~s', $query, $match, PREG_OFFSET_CAPTURE, $offset)) { //! respect sql_mode NO_BACKSLASH_ESCAPES
+					while (preg_match('(' . ($found == '/*' ? '\*/' : ($found == '[' ? ']' : (preg_match('~^-- |^#~', $found) ? "\n" : preg_quote($found) . "|\\\\."))) . '|$)s', $query, $match, PREG_OFFSET_CAPTURE, $offset)) { //! respect sql_mode NO_BACKSLASH_ESCAPES
 						$s = $match[0][0];
 						$offset = $match[0][1] + strlen($s);
 						if ($s[0] !== '\\') {
