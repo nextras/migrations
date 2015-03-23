603aed77f70108cb837b66114894397b9da775d6
Jan Tvrdik <jan.tvrdik@gmail.com>
2015-03-23 11:20:14 +0100

    added Symfony commands

---
+++ src/Bridges/SymfonyConsole/BaseCommand.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * This file is part of the Nextras community extensions of Nette Framework
+ *
+ * @license    New BSD License
+ * @link       https://github.com/nextras/migrations
+ */
+
+namespace Nextras\Migrations\Bridges\SymfonyConsole;
+
+use Nextras\Migrations\Engine\Runner;
+use Nextras\Migrations\Entities\Group;
+use Nextras\Migrations\Extensions;
+use Nextras\Migrations\IDriver;
+use Nextras\Migrations\Printers\Console;
+use Symfony\Component\Console\Command\Command;
+
+
+abstract class BaseCommand extends Command
+{
+
+	/** @var IDriver */
+	private $driver;
+
+	/** @var string */
+	private $dir;
+
+	/** @var array */
+	private $phpParams;
+
+
+	/**
+	 * @param  IDriver $driver
+	 * @param  string  $dir
+	 * @param  array   $phpParams (name => value)
+	 */
+	public function __construct(IDriver $driver, $dir, $phpParams = [])
+	{
+		parent::__construct();
+		$this->driver = $driver;
+		$this->dir = $dir;
+		$this->phpParams = $phpParams;
+	}
+
+
+	/**
+	 * @param  string $mode Runner::MODE_*
+	 * @param  bool   $withDummy include dummy data?
+	 * @return void
+	 */
+	protected function runMigrations($mode, $withDummy)
+	{
+		$printer = new Console();
+		$runner = new Runner($this->driver, $printer);
+
+		foreach ($this->getGroups($withDummy) as $group) {
+			$runner->addGroup($group);
+		}
+
+		foreach ($this->getExtensionHandlers() as $ext => $handler) {
+			$runner->addExtensionHandler($ext, $handler);
+		}
+
+		$runner->run($mode);
+	}
+
+
+	/**
+	 * @param  bool $withDummmy
+	 * @return Group[]
+	 */
+	protected function getGroups($withDummmy)
+	{
+		$structures = new Group();
+		$structures->enabled = TRUE;
+		$structures->name = 'structures';
+		$structures->directory = $this->dir . '/structures';
+		$structures->dependencies = [];
+
+		$basicData = new Group();
+		$basicData->enabled = TRUE;
+		$basicData->name = 'basic-data';
+		$basicData->directory = $this->dir . '/basic-data';
+		$basicData->dependencies = ['structures'];
+
+		$dummyData = new Group();
+		$dummyData->enabled = $withDummmy;
+		$dummyData->name = 'dummy-data';
+		$dummyData->directory = $this->dir . '/dummy-data';
+		$dummyData->dependencies = ['structures', 'basic-data'];
+
+		return [$structures, $basicData, $dummyData];
+	}
+
+
+	/**
+	 * @return array (extension => IExtensionHandler)
+	 */
+	protected function getExtensionHandlers()
+	{
+		return [
+			'sql' => new Extensions\SqlHandler($this->driver),
+			'php' => new Extensions\PhpHandler($this->phpParams),
+		];
+	}
+}
---
+++ src/Bridges/SymfonyConsole/CreateCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the Nextras community extensions of Nette Framework
+ *
+ * @license    New BSD License
+ * @link       https://github.com/nextras/migrations
+ */
+
+namespace Nextras\Migrations\Bridges\SymfonyConsole;
+
+use Nette;
+use Nette\Utils\Strings;
+use Nextras;
+use Nextras\Migrations\Extensions;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class CreateCommand extends BaseCommand
+{
+
+	protected function configure()
+	{
+		$this->setName('migrations:create');
+		$this->setDescription('Creates new migration file with proper name (e.g. 2015-03-14-130836-label.sql)');
+		$this->addArgument('type', InputArgument::REQUIRED, 's(tructures), b(asic-data) or d(ummy-data');
+		$this->addArgument('label', InputArgument::REQUIRED, 'short description');
+	}
+
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$dir = $this->getDirectory($input->getArgument('type'));
+		$name = $this->getFileName($input->getArgument('label'));
+		$path = "$dir/$name";
+		touch($path);
+	}
+
+
+	/**
+	 * @param  string $type
+	 * @return string
+	 */
+	private function getDirectory($type)
+	{
+		foreach ($this->getGroups(TRUE) as $group) {
+			if (Strings::startsWith($group->name, $type)) {
+				return $group->directory;
+			}
+		}
+
+		throw new Nextras\Migrations\LogicException("Unknown type '$type' given, expected on of 's', 'b' or 'd'.");
+	}
+
+
+	/**
+	 * @param  string $label
+	 * @return string
+	 */
+	private function getFileName($label)
+	{
+		return date('Y-m-d-His-') . Strings::webalize($label, '.') . '.sql';
+	}
+
+}
