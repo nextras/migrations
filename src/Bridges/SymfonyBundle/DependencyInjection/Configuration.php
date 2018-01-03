f07e52edecb4575fabfe60c5bc2b53e34a23bfa2
Ondra Votava <ja@ondravotava.cz>
2018-01-03 09:16:43 +0100

    SymfonyBundle: fix using deprecated cannotBeEmpty()

--- src/Bridges/SymfonyBundle/DependencyInjection/Configuration.php
+++ src/Bridges/SymfonyBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
 	public function getConfigTreeBuilder()
 	{
 		$treeBuilder = new TreeBuilder();
-		$treeBuilder->root('nextras_migrations')->cannotBeEmpty()->children()
+		$treeBuilder->root('nextras_migrations')->requiresAtLeastOneElement()->children()
 			->scalarNode('dir')
 				->defaultValue('%kernel.root_dir%/NextrasMigrations')
 				->cannotBeEmpty()
