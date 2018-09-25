454ffd4d5c1d8dc41664fd2d678b8f1b0ec72b3b
Jan Tvrdik <jan.tvrdik@gmail.com>
2018-09-25 03:45:18 +0200

    add test for newly supported Symfony 4

---
+++ tests/matrix/symfony-bundle/symfony-4.0.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export PHP_VERSION_MIN="70100"
+export PHP_VERSION_MAX="70299"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE doctrine/dbal:~2.0"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE doctrine/orm:~2.0"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE doctrine/doctrine-bundle"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE symfony/config:~4.0"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE symfony/console:~4.0"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE symfony/dependency-injection:~4.0"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE symfony/http-kernel:~4.0"
+export COMPOSER_REQUIRE="$COMPOSER_REQUIRE symfony/yaml:~4.0"
+export DBAL='doctrine'
