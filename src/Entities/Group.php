182df8575f8718f9cdf42cb0dea00fadb3e9665b
Jan Tvrdik <jan.tvrdik@gmail.com>
2019-01-20 10:52:34 +0100

    Revert "add IMigrationGroupsProvider"
    This reverts commit 2ed3dc0b6de07a4dd7496d83fe7c92834e26f084.

--- src/Entities/Group.php
+++ src/Entities/Group.php
@@ -21,80 +21,15 @@ class Group
 	public $name;
 
 	/** @var bool */
-	public $enabled = true;
+	public $enabled;
 
 	/** @var string absolute path do directory */
 	public $directory;
 
 	/** @var string[] */
-	public $dependencies = [];
+	public $dependencies;
 
 	/** @var IDiffGenerator|NULL */
 	public $generator;
 
-
-	/**
-	 * @param null|string $name
-	 * @param null|string $directory
-	 */
-	public function __construct($name = null, $directory = null)
-	{
-		$this->name = $name;
-		$this->directory = $directory;
-	}
-
-
-	/**
-	 * @param string $name
-	 * @return static
-	 */
-	public function setName($name)
-	{
-		$this->name = $name;
-		return $this;
-	}
-
-
-	/**
-	 * @param bool $enabled
-	 * @return static
-	 */
-	public function setEnabled($enabled)
-	{
-		$this->enabled = $enabled;
-		return $this;
-	}
-
-
-	/**
-	 * @param string $directory
-	 * @return static
-	 */
-	public function setDirectory($directory)
-	{
-		$this->directory = $directory;
-		return $this;
-	}
-
-
-	/**
-	 * @param string[] $dependencies
-	 * @return static
-	 */
-	public function setDependencies(array $dependencies)
-	{
-		$this->dependencies = $dependencies;
-		return $this;
-	}
-
-
-	/**
-	 * @param IDiffGenerator|NULL $generator
-	 * @return static
-	 */
-	public function setGenerator(IDiffGenerator $generator = NULL)
-	{
-		$this->generator = $generator;
-		return $this;
-	}
 }
