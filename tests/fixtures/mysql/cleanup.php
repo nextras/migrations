43f0405571c11f38fecb9d37ad15e10ed856c736
Jan Tvrdik <jan.tvrdik@gmail.com>
2020-12-19 14:50:19 +0100

    add strict_types

--- tests/fixtures/mysql/cleanup.php
+++ tests/fixtures/mysql/cleanup.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 return function() {
 	$this->dbal->exec('DROP DATABASE IF EXISTS ' . $this->dbal->escapeIdentifier($this->dbName));
--- tests/fixtures/pgsql/cleanup.php
+++ tests/fixtures/pgsql/cleanup.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 return function() {
 	$schema = $this->dbal->escapeIdentifier($this->dbName);
