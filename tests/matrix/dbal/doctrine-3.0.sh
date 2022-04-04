ac0adea6928a4069fe3ab8249ff4b828c33794ca
Jan Tvrdík <jan.tvrdik@gmail.com>
2022-04-04 17:13:14 +0200

    fix doctrine/dbal 3.x compatibility (#119)

---
+++ tests/matrix/dbal/doctrine-3.0.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+PHP_VERSION_MIN="70300"
+PHP_VERSION_MAX="80099"
+COMPOSER_REQUIRE="$COMPOSER_REQUIRE doctrine/dbal:~3.0"
+COMPOSER_REQUIRE="$COMPOSER_REQUIRE nette/tester:~2.3"
+DBAL="doctrine"
