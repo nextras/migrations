b9d2a4366de98889518f3267e66ee7580bad39ac
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-14 19:00:43 +0200

    readme: add guide for running integration tests in docker [closes #108]

---
+++ tests/run-in-docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -o errexit -o pipefail -o nounset
+
+export COMPOSER_HOME=${COMPOSER_HOME:-$HOME/.config/composer}
+export COMPOSER_CACHE_DIR=${COMPOSER_CACHE_DIR:-$HOME/.cache/composer}
+
+PHP_SERVICE="$1"
+shift
+
+docker-compose run \
+	-e COMPOSER_HOME \
+	-e COMPOSER_CACHE_DIR \
+	-v $COMPOSER_HOME:$COMPOSER_HOME \
+	-v $COMPOSER_CACHE_DIR:$COMPOSER_CACHE_DIR \
+	"$PHP_SERVICE" "$@"
